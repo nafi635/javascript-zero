b0fe90395be8104d727271b7f32bbe9a89355cb1
nafi635 <iamnafi44@gmail.com>
2022-12-12 09:06:38 +0530

    Arrays functions added.

---
+++ Arrays/arrays.js
@@ -0,0 +1,72 @@
+const numbers = [3,4, -1];
+
+function printNumbers() {
+    console.log(numbers);
+}
+
+//Adding numbers to the end
+numbers.push(5);
+printNumbers();
+
+//Adding numbers to the beginning
+numbers.unshift(4, 5);
+printNumbers();
+
+// numbers.splice(2,0,'a','n');
+// printNumbers();
+
+//Finding in an array - Index, indexof, lastindex of , 
+//includes method - just check whether array contain number
+console.log(numbers.includes(2));
+
+//Finding referrence types
+const courses = [
+    {id:1, firstName: 'a'},
+    {id:1, firstName: 'b'},
+]
+
+
+//array.find, findIndex method useful for referrence types searching
+const bCourse = courses.find(function(course) {
+    return course.firstName === 'b';
+})
+console.log(bCourse);
+
+
+const nums = [1,2,3];
+nums.length = 0;
+console.log(nums);
+
+//concat, slice method 
+numbers.concat(nums);
+printNumbers();
+
+//spread operator 
+const numbersCombined = [...numbers, ...nums];
+console.log(numbersCombined);
+
+const copy = [...numbersCombined];
+console.log(copy);
+
+//Iterating an array
+numbers.forEach((number,index) => console.log(index, number));
+
+//Every method - Return boolean value true or false
+const allPostive = numbers.every(function(value){
+    return value >=0;
+})
+
+console.log("Numbers ", numbers);
+console.log(allPostive);
+
+
+//Every method - Return array value true or false
+
+const postiveNumbersList = numbers.filter(function(value){
+    return value >=0;
+});
+
+const postiveNumbersLists = numbers.filter( number => number >2);
+
+console.log("Numbers ", numbers);
+console.log(postiveNumbersList);
--- Objects/objects.js
+++ Objects/objects.js
@@ -48,3 +48,93 @@ console.log(circleObject.draw);
 
 Circle.call({},1);
 
+//Primitive types and Reference types
+//Reference types - Objects arrays functions
+
+//Enumerating in an object
+
+for (let key in circle) {
+    console.log(key, circle[key]);
+}
+
+//For Of generally does'nt work for objects, but we can make convert object into array first
+for (let key of Object.keys(circle)) {
+    console.log(key, circle[key]);
+}
+
+if('radius' in circle) {
+    console.log("Radius property there in circle");
+} 
+
+
+//Cloning an object
+const anotherCircle = {}
+for(let key in circle){
+    anotherCircle[key]=circle[key];
+}
+
+console.log(anotherCircle);
+
+//Object. assign method directly cloning an object
+const another = Object.assign({}, circle);
+console.log(another);
+
+
+//Using spread operator
+const anotherObject = { ...circle };
+console.log("Anothehr object cloned by spread operator", anotherObject); 
+
+//Garbage collector
+//Javascript same as Java - Automatic garbage collector
+
+
+// Built in objects
+//Math object
+const maxNumbe= Math.max(1,3,4,5);
+console.log("Max number "+ maxNumbe);
+
+//String Object
+const stringone =new String("  Hi how are you  ");
+console.log("String functions " +stringone.trim())
+
+//Esacape Notations
+console.log("This is \' single quoted");
+console.log("This is \n new lined quoted");
+
+
+//Template literals - we use the backtick ( ` ) character to print exactly same to the printer
+const templateLiteralString = `Template 
+literal 
+whatever`;
+console.log(templateLiteralString);
+
+const nafiName = "Nafi";
+const hiNafi = `Hi ${nafiName}
+ how are you ? `
+console.log(hiNafi);
+
+//Date built in object
+const date1 = new Date();
+console.log(date1.getDay());
+
+//Playing with address object
+function Address(street, city, zipcode) {
+    this.street = street;
+    this.city = city;
+    this.zipcode = zipcode;
+}
+
+function areEqual(address1, address2) {
+    return address1.street == address2.street &&
+    address1.city == address2.city &&
+    address1.zipcode == address2.zipcode 
+}
+
+function areSame(address1, address2) {
+    return address1 === address2;
+}
+
+let address1 = new Address('a', 'b', 'c');
+let address2 = new Address('a', 'b', 'c');
+console.log("Address equal " + areEqual(address1,address2));
+console.log("Address equal " + areSame(address1,address2));  // it returns false as two different objects, two different memory
